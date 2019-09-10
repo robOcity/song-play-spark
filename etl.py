faa68c28e79dc288b3b11763248a845dc20a4b23
robOcity <robert.osterburg@gmail.com>
2019-09-10 10:40:29 -0600

    feat: initial pyspark code running

--- etl.py
+++ etl.py
@@ -1,16 +1,10 @@
 import configparser
 from datetime import datetime
 import os
+from pathlib import Path
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import udf, col
+from pyspark.sql import functions as F, types as T
 from pyspark.sql.functions import year, month, dayofmonth, hour, weekofyear, date_format
-
-
-config = configparser.ConfigParser()
-config.read('dl.cfg')
-
-os.environ['AWS_ACCESS_KEY_ID']=config['AWS_ACCESS_KEY_ID']
-os.environ['AWS_SECRET_ACCESS_KEY']=config['AWS_SECRET_ACCESS_KEY']
 
 
 def create_spark_session():
@@ -22,69 +16,77 @@ def create_spark_session():
 
 
 def process_song_data(spark, input_data, output_data):
-    # get filepath to song data file
-    song_data = 
-    
-    # read song data file
-    df = 
 
-    # extract columns to create songs table
-    songs_table = 
-    
-    # write songs table to parquet files partitioned by year and artist
-    songs_table
+    # read the song data file into a dataframe
+    songs_df = spark.read.json(Path('./data') / 'song-data.zip')
+    print(songs_df.printSchema())
 
-    # extract columns to create artists table
-    artists_table = 
+    # extract columns from the songs dataframe
+    songs_df = songs_df.select(['song_id', 'title', 'artist_id', 'year', 'duration'])
     
-    # write artists table to parquet files
-    artists_table
+    # write songs dataframe to parquet files partitioned by year and artist
+    songs_df.write.parquet(Path('.data/') / 'star-schema' / 'tables')
+
+    # # extract columns to create artists table
+    # artists_table = 
+    
+    # # write artists table to parquet files
+    # artists_table
 
 
 def process_log_data(spark, input_data, output_data):
-    # get filepath to log data file
-    log_data =
+    pass
+    # # get filepath to log data file
+    # log_data =
 
-    # read log data file
-    df = 
+    # # read log data file
+    # df = 
     
-    # filter by actions for song plays
-    df = 
+    # # filter by actions for song plays
+    # df = 
 
-    # extract columns for users table    
-    artists_table = 
+    # # extract columns for users table    
+    # artists_table = 
     
-    # write users table to parquet files
-    artists_table
+    # # write users table to parquet files
+    # artists_table
 
-    # create timestamp column from original timestamp column
-    get_timestamp = udf()
-    df = 
+    # # create timestamp column from original timestamp column
+    # get_timestamp = udf()
+    # df = 
     
-    # create datetime column from original timestamp column
-    get_datetime = udf()
-    df = 
+    # # create datetime column from original timestamp column
+    # get_datetime = udf()
+    # df = 
     
-    # extract columns to create time table
-    time_table = 
+    # # extract columns to create time table
+    # time_table = 
     
-    # write time table to parquet files partitioned by year and month
-    time_table
+    # # write time table to parquet files partitioned by year and month
+    # time_table
 
-    # read in song data to use for songplays table
-    song_df = 
+    # # read in song data to use for songplays table
+    # song_df = 
 
-    # extract columns from joined song and log datasets to create songplays table 
-    songplays_table = 
+    # # extract columns from joined song and log datasets to create songplays table 
+    # songplays_table = 
 
-    # write songplays table to parquet files partitioned by year and month
-    songplays_table
+    # # write songplays table to parquet files partitioned by year and month
+    # songplays_table
 
 
 def main():
+    config = configparser.ConfigParser()
+    config.read('dl.cfg')
+
+    os.environ['AWS_ACCESS_KEY_ID']=config['AWS']['AWS_ACCESS_KEY_ID']
+    os.environ['AWS_SECRET_ACCESS_KEY']=config['AWS']['AWS_SECRET_ACCESS_KEY']
+
     spark = create_spark_session()
-    input_data = "s3a://udacity-dend/"
-    output_data = ""
+    input_data = "./data/logs"
+    output_data = "./data/star-tables"
+    # input_data = "s3a://udacity-dend/"
+    # output_data = ""
     
     process_song_data(spark, input_data, output_data)    
     process_log_data(spark, input_data, output_data)
